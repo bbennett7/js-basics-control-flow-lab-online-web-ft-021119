70e4253d9a0891c52e6009719123842b21bfb89f
bbennett7 <bryn.bennett@gmail.com>
2019-05-21 20:03:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let greeting
+
+  if (distance <= 400) {
+    greeting = 'This one is on me!';
+  } else if (distance >= 2500) {
+    greeting = 'No can do.';
+  } else if (distance >= 2000) {
+    greeting = 'I will gladly take your thirty bucks.';
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  let response
+
+  if (city === 'NYC') {
+    response = 'Ok, sounds good.';
+  } else {
+    response = 'No go.';
+  }
+return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let response
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response
 }
